a358cb153a85e056221e34595b469ca81ef6aaf9
Abraham E. Tavarez <abetavarez@icloud.com>
2020-04-10 11:08:20 -0400

    MVP

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
-console.log('Running Scripts...')
+// console.log('Running Scripts...')
 
-//Constants
+//* Constants
 const DOMAIN = 'https://www.themealdb.com/api/json/v1/';
 const API_KEY = 1;
 const BASE_URL = `${DOMAIN}${API_KEY}/search.php?s=`;
@@ -8,18 +8,7 @@ const SearchByFirstLetter_BASE_URL = `${DOMAIN}${API_KEY}/search.php?f=`;
 const GET_RANDOM_MEAL_BASE_URL = `${DOMAIN}${API_KEY}/random.php`;
 const SearchByID_BASE_URL = `${DOMAIN}${API_KEY}/lookup.php?i=`;
 
-//by meal
-// 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata'
-//by first letter
-//https://www.themealdb.com/api/json/v1/1/search.php?f=a
-//by id
-//https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
-//get random
-//https://www.themealdb.com/api/json/v1/1/random.php
-
-
-//*DOM Variables
-
+//* DOM Variables
 const randomMealDIV = document.getElementById('random-div');
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
@@ -29,28 +18,19 @@ async function getRandomMealData() {
   try {
     //randomMeal obj of random meal
     let randomMeal = await axios.get(`${GET_RANDOM_MEAL_BASE_URL}`);
-
     // console.log('data---->', randomMeal)
-
-
     const meal = randomMeal.data.meals[0];
     // console.log('meal--->', meal.strMeal);
-
     renderRandomMeal(meal)
-
   } catch (error) {
     console.error(error)
   };
-
 };
 getRandomMealData();
-// setInterval(getRandomMealData, 2000);
 
 //* Render the random meal to top of the page
 function renderRandomMeal(meal) {
   randomMealDIV.innerHTML = '';
-  // console.log(randomMeal.data.meals['0'].strMeal)
-  // console.log('Here --->', randomMeal.data.meals['0'].strMealThumb)
 
   //Meal variables
   const imgURL = meal.strMealThumb;
@@ -64,13 +44,9 @@ function renderRandomMeal(meal) {
   img.setAttribute('src', imgURL);
   img.setAttribute('class', 'display-random')
   //appending to the dom
-
   randomMealDIV.appendChild(h3);
   randomMealDIV.appendChild(img);
-
 };
-// renderRandomMeal(randomMealsArray);
-
 
 //* Event Handlers for search meals
 async function searchMeal(value) {
@@ -83,9 +59,6 @@ async function searchMeal(value) {
   //Get Request
   try {
     let result = await axios.get(`${BASE_URL}${value}`);
-    // console.log('Result Data --->', result);
-    // console.log('here--->', result.data.meals[0].strIngredient1)
-
     renderResults(result);
     searchInput.value = '';
   } catch (error) {
@@ -97,40 +70,36 @@ async function searchMeal(value) {
 //* Renders Search Results
 function renderResults(result) {
   //Data
-  let meal = result.data.meals; //here
-  // console.log(meal[1])
+  let meal = result.data.meals;
 
   for (let x = 0; x < meal.length; x++) {
     console.log(meal[x])
-
     // Main DIV
     let resultsDiv = document.getElementById('search-results');
     resultsDiv.setAttribute('class', 'result');
-    // resultsDiv.innerHTML = '';
-    // if (result.data.meals == null) {
-    //   console.log('No Matches')
-    //   const h2NoResult = document.createElement('h2');
-    //   h2NoResult.innerHTML = 'No matches';
-    //   resultsDiv.appendChild(h2NoResult);
-    // };
+    if (result.data.meals == null) {
+      console.log('No Matches')
+      const h2NoResult = document.createElement('h2');
+      h2NoResult.innerHTML = 'No matches';
+      resultsDiv.appendChild(h2NoResult);
+    };
     ////////////////////////////////
     // Meal title
     let h2MealName = document.createElement('h1');
     h2MealName.innerHTML = meal[x].strMeal;
-    // h2MealName.innerHTML = result.data.meals[x].strMeal; //here
     resultsDiv.appendChild(h2MealName);
+
+    let videoURL = meal[x].strYoutube;
+    let link = document.createElement('a');
+    link.setAttribute('href', videoURL);
+    link.innerHTML = 'watch video';
+    resultsDiv.appendChild(link)
     //create append img
     let img = document.createElement('img');
     let imgURL = meal[x].strMealThumb; //here
-
     img.setAttribute('src', imgURL);
     img.setAttribute('class', 'result');
     resultsDiv.appendChild(img);
-
-
-
-
-
     // UL Display Ingredients and Measuments
     let ulResult = document.createElement('ul');
     resultsDiv.appendChild(ulResult);
@@ -138,44 +107,28 @@ function renderResults(result) {
     let h2Ingredients = document.createElement('h2');
     h2Ingredients.innerHTML = 'Ingredients';
     ulResult.appendChild(h2Ingredients);
-
     // Built Object
-    let ingArr = [];
-    let measArr = [];
-    let ingMeasObjArr = [];
     //Loops on ingredients creates lis appends them to ul
     for (i in meal[x]) { //here
       if (i.includes("Ing") && meal[x][i] != "" && meal[x][i] != " ") {
-
         let li = document.createElement('li');
         li.innerHTML = meal[x][i];
         ulResult.appendChild(li)
-
-        // console.log('ing---->', meal[i])
-        // ingArr.push(meal[i]);
       };
     };
-
-
     //Creates H2 to display Measurements
     let h2Meas = document.createElement('h2');
     h2Meas.innerHTML = 'Measurements';
     //apends it to ul
     ulResult.appendChild(h2Meas);
-
     for (m in meal[x]) {
       if (m.includes("Meas") && meal[x][m] != "" && meal[x][m] != " ") {
-
         let li = document.createElement('li');
         li.innerHTML = meal[x][m];
         ulResult.appendChild(li);
-        // console.log('mes---->', meal[m])
-        // measArr.push(meal[x][m]);
       };
     };
   }
 }
-
-
 //* SEARCH BAR
 searchButton.addEventListener('click', searchMeal);
