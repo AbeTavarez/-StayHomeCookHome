f2c7f62b959b84040c0ab0fde6cd33b54dd60194
Abraham E. Tavarez <abetavarez@icloud.com>
2020-07-28 19:36:18 -0400

    completed transfer

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,18 @@
 // console.log('Running Scripts...')
 
 //* Constants
-const DOMAIN = 'https://www.themealdb.com/api/json/v1/';
+const DOMAIN = "https://www.themealdb.com/api/json/v1/";
 const API_KEY = 1;
 const BASE_URL = `${DOMAIN}${API_KEY}/search.php?s=`;
 const SearchByFirstLetter_BASE_URL = `${DOMAIN}${API_KEY}/search.php?f=`;
 const GET_RANDOM_MEAL_BASE_URL = `${DOMAIN}${API_KEY}/random.php`;
 const SearchByID_BASE_URL = `${DOMAIN}${API_KEY}/lookup.php?i=`;
+const SearchByMainIngredient = `${DOMAIN}${API_KEY}/filter.php?i=`;
 
 //* DOM Variables
-const randomMealDIV = document.getElementById('random-div');
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
+const randomMealDIV = document.getElementById("random-div");
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
 
 //* Gets random meal from DB
 async function getRandomMealData() {
@@ -21,50 +22,49 @@ async function getRandomMealData() {
     // console.log('data---->', randomMeal)
     const meal = randomMeal.data.meals[0];
     // console.log('meal--->', meal.strMeal);
-    renderRandomMeal(meal)
+    renderRandomMeal(meal);
   } catch (error) {
-    console.error(error)
-  };
-};
+    console.error(error);
+  }
+}
 getRandomMealData();
 
 //* Render the random meal to top of the page
 function renderRandomMeal(meal) {
-  randomMealDIV.innerHTML = '';
+  randomMealDIV.innerHTML = "";
   //Meal variables
   const imgURL = meal.strMealThumb;
   const mealName = meal.strMeal;
   //HTML Elements
-  const h3 = document.createElement('h2');
-  const img = document.createElement('img');
+  const h3 = document.createElement("h2");
+  const img = document.createElement("img");
   //setting attributes
   h3.innerText = mealName;
   // para.setAttribute('class', 'random-h ')
-  img.setAttribute('src', imgURL);
-  img.setAttribute('class', 'display-random')
+  img.setAttribute("src", imgURL);
+  img.setAttribute("class", "display-random");
   //appending to the dom
   randomMealDIV.appendChild(h3);
   randomMealDIV.appendChild(img);
-};
+}
 
 //* Event Handlers for search meals
 async function searchMeal(value) {
   //Check for value
-  if (searchInput.value === 'undefined') {
-    console.log('search input is empty dont do anything')
+  if (searchInput.value === "undefined") {
+    console.log("search input is empty dont do anything");
   } else {
-    value = searchInput.value
+    value = searchInput.value;
   }
   //Get Request
   try {
     let result = await axios.get(`${BASE_URL}${value}`);
     renderResults(result);
-    searchInput.value = '';
+    searchInput.value = "";
   } catch (error) {
     console.error(error);
-  };
-
-};
+  }
+}
 
 //* Renders Search Results
 function renderResults(result) {
@@ -72,62 +72,69 @@ function renderResults(result) {
   let meal = result.data.meals;
 
   for (let x = 0; x < meal.length; x++) {
-    console.log(meal[x])
+    console.log(meal[x]);
     // Main DIV
-    let resultsDiv = document.getElementById('search-results');
-    resultsDiv.setAttribute('class', 'result');
+    let resultsDiv = document.getElementById("search-results");
+    resultsDiv.setAttribute("class", "result");
     if (result.data.meals == null) {
-      console.log('No Matches')
-      const h2NoResult = document.createElement('h2');
-      h2NoResult.innerHTML = 'No matches';
+      console.log("No Matches");
+      const h2NoResult = document.createElement("h2");
+      h2NoResult.innerHTML = "No matches";
       resultsDiv.appendChild(h2NoResult);
-    };
+    }
     ////////////////////////////////
     // Meal title
-    let h2MealName = document.createElement('h1');
+    let h2MealName = document.createElement("h1");
     h2MealName.innerHTML = meal[x].strMeal;
     resultsDiv.appendChild(h2MealName);
 
     let videoURL = meal[x].strYoutube;
-    let link = document.createElement('a');
-    link.setAttribute('href', videoURL);
-    link.innerHTML = 'watch video';
-    resultsDiv.appendChild(link)
+    let link = document.createElement("a");
+    link.setAttribute("href", videoURL);
+    link.innerHTML = "watch video";
+    resultsDiv.appendChild(link);
     //create append img
-    let img = document.createElement('img');
+    let img = document.createElement("img");
     let imgURL = meal[x].strMealThumb; //here
-    img.setAttribute('src', imgURL);
-    img.setAttribute('class', 'result');
+    img.setAttribute("src", imgURL);
+    img.setAttribute("class", "result");
     resultsDiv.appendChild(img);
     // UL Display Ingredients and Measuments
-    let ulResult = document.createElement('ul');
+    let ulResult = document.createElement("ul");
     resultsDiv.appendChild(ulResult);
     //H2 to display Ingredients
-    let h2Ingredients = document.createElement('h2');
-    h2Ingredients.innerHTML = 'Ingredients';
+    let h2Ingredients = document.createElement("h2");
+    h2Ingredients.innerHTML = "Ingredients";
     ulResult.appendChild(h2Ingredients);
     // Built Object
     //Loops on ingredients creates lis appends them to ul
-    for (i in meal[x]) { //here
+    for (i in meal[x]) {
+      //here
       if (i.includes("Ing") && meal[x][i] != "" && meal[x][i] != " ") {
-        let li = document.createElement('li');
+        let li = document.createElement("li");
         li.innerHTML = meal[x][i];
-        ulResult.appendChild(li)
-      };
-    };
+        ulResult.appendChild(li);
+      }
+    }
     //Creates H2 to display Measurements
-    let h2Meas = document.createElement('h2');
-    h2Meas.innerHTML = 'Measurements';
+    let h2Meas = document.createElement("h2");
+    h2Meas.innerHTML = "Measurements";
     //apends it to ul
     ulResult.appendChild(h2Meas);
     for (m in meal[x]) {
       if (m.includes("Meas") && meal[x][m] != "" && meal[x][m] != " ") {
-        let li = document.createElement('li');
+        let li = document.createElement("li");
         li.innerHTML = meal[x][m];
         ulResult.appendChild(li);
-      };
-    };
+      }
+    }
   }
 }
 //* SEARCH BAR
-searchButton.addEventListener('click', searchMeal);
+searchButton.addEventListener("click", searchMeal);
+searchInput.addEventListener("keyup", function () {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("search-button").click();
+  }
+});
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Project Overview
+# Stay Home Cook Home
 
-## Project Name
+[![Netlify Status](https://api.netlify.com/api/v1/badges/910d26dd-1614-447b-b4d3-a642b6e39b95/deploy-status)](https://app.netlify.com/sites/vibrant-yonath-0cbbfa/deploys)
 
-StayHomeCookHome
-(https://pages.git.generalassemb.ly/AbrahamEfrenTavarez/StayHomeCookHome/ "Page link")
+#StayHomeCookHome
+(https://www.stayhomecookhome.com/)
 
 ## Project Description
 
@@ -15,15 +15,15 @@ Now that you're working from home, this is the best time to learn how to cook.
 
 - Search thousands of recipes on the website and get a full list of ingredients, and a short how-to video.
 
-## Api and Data Sample
+## API and Data Sample
 
 #### The Meal DB API
 
-The MealDB site:
+This site is build with The Meal DB API:
 (https://www.themealdb.com/api.php)
 ![alt text](https://res.cloudinary.com/abetavarez/image/upload/v1586143923/Screen_Shot_2020-04-05_at_11.30.23_PM_e5cyb5.png "JSON DATA")
 
-#### JSON Data
+#### JSON Data Smaple
 
 #### Postman
 
@@ -38,59 +38,6 @@ The MealDB site:
 ### Mobile
 
 ![alt text](https://res.cloudinary.com/abetavarez/image/upload/v1586180975/Screen_Shot_2020-04-06_at_9.49.11_AM_rnbqpw.png "wireframe image mobile")
-
-## MVP/Post MVP
-
-### MVP
-
-- Find an external API to access food recipes data.
-- Render data from API on the page.
-- Style the page as indicated on the wireframe.
-- Add search funtionallity, allowing the user to search recipes.
-- Render results on the page with images and recipe ingredients.
-
----
-
-### Post MVP
-
-- Render random recipes on the left column of the page.
-- Add search categories on the right column of the page.
-- Add short video link to recipes.
-- Use local storage to save favorites.
-
-## Project Schedule
-
-| Day             | Deliverable                                          | Status   |
-| --------------- | ---------------------------------------------------- | -------- |
-| April 2rd       | Project Prompt                                       | Complete |
-| April 3rd - 5th | Wireframes / Priority Matrix                         | Complete |
-| April 6th       | Project Aproval/Core Application Structure(HTML,CSS) | Complete |
-| April 7th       | Pseudocode / API and JS functionality                | Complete |
-| April 8th       | Clickable Model Search, Rendering functionality      | Complete |
-| April 9th       | MVP                                                  | Complete |
-| April 10th      | Present                                              | Complete |
-
-## Priority Matrix
-
-## ![alt text](https://res.cloudinary.com/abetavarez/image/upload/v1586146746/Screen_Shot_2020-04-06_at_12.18.45_AM_nphuft.png "priority matrix")
-
-![alt text](https://res.cloudinary.com/abetavarez/image/upload/v1586146445/Screen_Shot_2020-04-05_at_10.43.38_PM_bqrwru.png "priority matrix")
-
-## Timeframes
-
-| Component                           | Priority | Estimated Time | Time Invested | Actual Time |
-| ----------------------------------- | :------: | :------------: | :-----------: | :---------: |
-| Research APIs                       |    L     |      4hrs      |     4hrs      |    4hrs     |
-| Wireframes/Priority Matrix          |    L     |      3hrs      |     3hrs      |    3hrs     |
-| Testing API & JSON Data             |    M     |      3hrs      |     3hrs      |    3hrs     |
-| Basic HTML and Basic CSS            |    M     |      3hrs      |     3hrs      |    3hrs     |
-| Setting up API, CDNs and Test Calls |    M     |      4hrs      |     4hrs      |    4hrs     |
-| JavaScript DOM Manipulation         |    H     |      4hrs      |     4hrs      |    4hrs     |
-| Testing and Debugging               |    H     |      4hrs      |     2hrs      |    2hrs     |
-| JS Functions and Handlers           |    H     |      5hrs      |     8hrs      |    8hrs     |
-| Applys CSS FlexBox                  |    H     |      4hrs      |     4hrs      |    4hrs     |
-| Site interactivity JS & FlexBox     |    H     |      6hrs      |     6hrs      |    6hrs     |
-| Total                               |    H     |     40hrs      |     41hrs     |    41hrs    |
 
 ## Code Snippet
 
